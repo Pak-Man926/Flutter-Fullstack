ba66ff4841c08117e2e64ec48642123fd837bacf
Pak_Man926 <davidkamau926@gmail.com>
2025-05-07 10:47:34 +0300

    Main Page linking

--- myproject/myproject_flutter/lib/main.dart
+++ myproject/myproject_flutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:myproject_client/myproject_client.dart';
 import 'package:flutter/material.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 import "package:myproject_flutter/src/serverpod_client.dart";
+import "package:myproject_flutter/src/widgets/sign_in_page.dart";
 
 void main() async
 {
@@ -9,10 +10,44 @@ void main() async
   
   await initializeServerpodClient();
 
-  runApp(constMyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget
 {
-  
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: "Serverpod Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: "Serverpod example"),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget
+{
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text(widget.title),
+      ),
+      body: SignInPage(),
+    );
+  }
 }
