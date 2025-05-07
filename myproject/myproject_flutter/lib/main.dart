b6b6c4167b396c68ae98bb8f34e0f66112a516fc
Pak_Man926 <davidkamau926@gmail.com>
2025-05-07 10:50:55 +0300

    Client error fixing

--- myproject/myproject_flutter/lib/main.dart
+++ myproject/myproject_flutter/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:myproject_client/myproject_client.dart';
+//import 'package:myproject_client/myproject_client.dart';
 import 'package:flutter/material.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
+//import 'package:serverpod_flutter/serverpod_flutter.dart';
 import "package:myproject_flutter/src/serverpod_client.dart";
 import "package:myproject_flutter/src/widgets/sign_in_page.dart";
 
--- myproject/myproject_flutter/lib/src/serverpod_client.dart
+++ myproject/myproject_flutter/lib/src/serverpod_client.dart
@@ -11,7 +11,7 @@ Future<void> initializeServerpodClient() async
   const ipAddress = "localhost";
 
   client = Client(
-    "http://$ipAddress:8080",
+    "http://$ipAddress:8080/",
     authenticationKeyManager: FlutterAuthenticationKeyManager(),
   )..connectivityMonitor = FlutterConnectivityMonitor();
 
