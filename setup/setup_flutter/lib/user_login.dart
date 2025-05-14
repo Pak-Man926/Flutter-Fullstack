a463a6cf3b5ea0d9bb64a6170361a7c96dddace3
Pak_Man926 <davidkamau926@gmail.com>
2025-05-14 11:57:14 +0300

    Login Setup

--- setup/setup_flutter/lib/user_login.dart
+++ setup/setup_flutter/lib/user_login.dart
@@ -8,7 +8,39 @@ class Login extends StatelessWidget
   Widget build(BuildContext context)
   {
     return Scaffold(
-      body: 
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Contact Number',
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Call to the endpoint to login user
+              },
+              child: Text('Login'),
+            ),
+          ]
+        )
+      )
     );
   }
 }
