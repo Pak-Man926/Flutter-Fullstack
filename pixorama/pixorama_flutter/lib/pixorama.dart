f092dd7b7fd3839fefc167f40336f36e3b8889ce
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 17:17:00 +0300

    Auto commit

--- pixorama/pixorama_flutter/lib/pixorama.dart
+++ pixorama/pixorama_flutter/lib/pixorama.dart
@@ -34,7 +34,7 @@ class _PixoramaState extends State<Pixorama>
         if(update is ImageDate)
         {
           setState((){
-            _
+            _imageController =
           })
         }
       }
