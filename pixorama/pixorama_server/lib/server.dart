89e954f29dbbb77618e4f9acc210fca43a663022
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 15:26:04 +0300

    Auto commit

--- pixorama/pixorama_server/lib/server.dart
+++ pixorama/pixorama_server/lib/server.dart
@@ -2,7 +2,6 @@ import 'package:serverpod/serverpod.dart';
 
 import 'package:pixorama_server/src/web/routes/root.dart';
 
-
 // This is the starting point of your Serverpod server. In most cases, you will
 // only need to make additions to this file if you add future calls,  are
 // configuring Relic (Serverpod's web-server), or need custom setup work.
