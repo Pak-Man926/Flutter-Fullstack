0f44814506feefc70b0b0734cd0e00f8e180ab76
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 16:34:47 +0300

    Auto commit

--- pixorama/pixorama_server/lib/src/endpoints/pixorama_endpoint.dart
+++ pixorama/pixorama_server/lib/src/endpoints/pixorama_endpoint.dart
@@ -43,7 +43,7 @@ class PixoramaEndpoint extends Endpoint
 
    Stream imageUpdate(Session session) async*
    {
-    var updateStream = session.messages.crea
+    var updateStream = session.messages.create
    }
 
    
