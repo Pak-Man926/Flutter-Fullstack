a7eda35e3dbf46b0b911be7a10e276b87360864b
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 16:19:57 +0300

    Auto commit

--- pixorama/pixorama_server/lib/src/endpoints/pixorama_endpoint.dart
+++ pixorama/pixorama_server/lib/src/endpoints/pixorama_endpoint.dart
@@ -16,5 +16,5 @@ class PixoramaEndpoint extends Endpoint
     _numPixels,
      _defaultPixelColor);
 
-   static const _channelPixelAdded = "pixel_added"  
+   static const _channelPixelAdded = "pixel_added";  
 }
