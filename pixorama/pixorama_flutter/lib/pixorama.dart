a4978fe82446c5ee8b0fa4a0a5426986e8ca8f74
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 17:27:15 +0300

    Auto commit

--- pixorama/pixorama_flutter/lib/pixorama.dart
+++ pixorama/pixorama_flutter/lib/pixorama.dart
@@ -42,7 +42,7 @@ class _PixoramaState extends State<Pixorama>
           _imageController?.setPixelIndex(pixelIndex: update.pixelIndex, colorIndex: update.colorIndex);
         }
       } on MethodStreamException catch(_)
-    {
+        {
       setState(() {
         _imageController = null;
       });
