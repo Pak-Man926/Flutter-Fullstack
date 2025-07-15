0092274829f5e04bee87852e8a466c199f1cfd0a
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 16:23:06 +0300

    Auto commit

--- pixorama/pixorama_server/lib/src/endpoints/pixorama_endpoint.dart
+++ pixorama/pixorama_server/lib/src/endpoints/pixorama_endpoint.dart
@@ -22,7 +22,7 @@ class PixoramaEndpoint extends Endpoint
    {
     if(colorIndex < 0 || colorIndex >= _numColorsInPalette)
     {
-      throw FormatException("")
+      throw FormatException("Color")
     }
    }
 }
