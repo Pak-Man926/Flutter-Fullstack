32eecad597153cadfbe1c59c8a60f387042d1aef
Pak_Man926 <davidkamau926@gmail.com>
2025-07-16 01:50:48 +0300

    Creating an interface

--- pixorama/pixorama_flutter/lib/main.dart
+++ pixorama/pixorama_flutter/lib/main.dart
@@ -2,7 +2,7 @@ import "package:pixorama_client/pixorama_client.dart";
 import "package:flutter/material.dart";
 import "package:serverpod_flutter/serverpod_flutter.dart";
 
-import "src/pixorama.dart";
+import "package:pixorama_flutter/pixorama.dart";
 
 var client = Client("http://$localhost:8080/")
   ..connectivityMonitor = FlutterConnectivityMonitor();
--- pixorama/pixorama_flutter/lib/pixorama.dart
+++ pixorama/pixorama_flutter/lib/pixorama.dart
@@ -3,51 +3,64 @@ import "package:pixels/pixels.dart";
 import "package:pixorama_client/pixorama_client.dart";
 import "../../main.dart";
 
-class Pixorama extends StatefulWidget
-{
+class Pixorama extends StatefulWidget {
   const Pixorama({super.key});
 
   @override
   State<Pixorama> createState() => _PixoramaState();
 }
 
-class _PixoramaState extends State<Pixorama>
-{
+class _PixoramaState extends State<Pixorama> {
   PixelImageController? _imageController;
 
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
 
     _listenToUpdates();
   }
 
-  Future<void> _listenToUpdates() async
-  {
-    while(true)
-    {
-      final imageUpdates = client.pixorama.imageUpdate();
+  Future<void> _listenToUpdates() async {
+    while (true) {
+      try {
+        final imageUpdates = client.pixorama.imageUpdate();
 
-      await for(final update in imageUpdates)
-      {
-        if(update is ImageDate)
-        {
-          setState((){
-            _imageController = PixelImageController(palette: PixelPalette.rPlace(), width: update.width, height: update.height, pixels: update.pixels);
-          });
-        }
-        else if(update is ImageUpdate)
-        {
-          _imageController?.setPixelIndex(pixelIndex: update.pixelIndex, colorIndex: update.colorIndex);
-        }
-      } on MethodStreamException catch(_)
-        {
-          setState(() {
-            _imageController = null;
-           });
+        await for (final update in imageUpdates) {
+          if (update is ImageData) {
+            setState(() {
+              _imageController = PixelImageController(
+                  palette: PixelPalette.rPlace(),
+                  width: update.width,
+                  height: update.height,
+                  pixels: update.pixels);
+            });
+          } else if (update is ImageUpdate) {
+            _imageController?.setPixelIndex(
+                pixelIndex: update.pixelIndex, colorIndex: update.colorIndex);
           }
-     } 
-     await Future.delayed(Duration(seconds:5));
+        }
+      } on MethodStreamException catch (_) {
+        setState(() {
+          _imageController = null;
+        });
+      }
+      await Future.delayed(Duration(seconds:5));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Center(
+      child: _imageController == null
+          ? const CircularProgressIndicator()
+          : PixelEditor(
+            controller: _imageController!,
+            onSetPixel: (details) {
+              client.pixorama.setPixel(colorIndex: details.colorIndex, pixelIndex: details.tapDetails.index);
+            }
+            ),
+
+    );
   }
 }
