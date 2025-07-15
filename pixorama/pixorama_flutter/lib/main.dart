5d37c7e3d91c6c16841bed0e067e44823777308b
Pak_Man926 <davidkamau926@gmail.com>
2025-07-15 16:59:14 +0300

    Auto commit

--- pixorama/pixorama_flutter/lib/main.dart
+++ pixorama/pixorama_flutter/lib/main.dart
@@ -4,5 +4,4 @@ import "package:serverpod_flutter/serverpod_flutter.dart";
 
 import "src/pixorama.dart";
 
-var xlient = Client("http://$localhost:8080/")
-  
+var client = Client("http://$localhost:8080/")
